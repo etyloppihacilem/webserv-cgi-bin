ae3c15d92d742ab92fe0683ab9437fad8011fa49
hmelica <hippolytemelica@icloud.com>
2024-07-30 10:14:58 +0200

    some really simple CGI programs

---
+++ perl/index.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+print("Hello World in perl !\n");
+$datestring = localtime();
+print "Local date and time $datestring\n";
---
+++ php/index.php
@@ -0,0 +1,4 @@
+<?php
+    echo "Hello world in php !";
+    echo date("D, d M Y H:i:s");
+?>

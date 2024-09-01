9eb4d5f0e0c5b41c902796b4bd156332ff3545fe
hmelica <hippolytemelica@icloud.com>
2024-09-01 14:26:56 +0200

    python log

--- python/index.py
+++ python/index.py
@@ -1,3 +1,7 @@
+with open("python.log", "a") as f:
+    f.write("Coucou lancé :)\n")
 print("Content-type: text/html; charset=UTF-8", end="\r\n\r\n")
 
-print("Hello world in python !") # this should do more stuff as post requests are supported...
+print("Hello world in python !", end="") # this should do more stuff as post requests are supported...
+with open("python.log", "a") as f:
+    f.write("Coucou fini :)\n")
